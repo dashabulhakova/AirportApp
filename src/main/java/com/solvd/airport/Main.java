6b4621902b0031484673be71b3f2d1347d724bd0
dashabull <dasha.bull@gmail.com>
2022-10-25 19:57:59 -0400

    added model

--- src/main/java/com/solvd/airport/Main.java
+++ src/main/java/com/solvd/airport/Main.java
@@ -16,33 +16,27 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
         UserService userService = new UserService();
-        //jdbc implementation
+
         User newUser = new User(4, "Cara", "Styles", "Lara@email.com");
-        //userService.createUser(newUser);
-        /*newUser = userService.getUserByID(4);
+        userService.createUser(newUser);
+        newUser = userService.getUserByID(4);
         LOGGER.info(newUser);
         newUser.setFirstName("Mary");
         userService.updateUser(newUser);
         userService.getUserByID(4);
         userService.getAllUsers();
-*/
-        //myBatis implementation
+
+
         IStaffService staffService = new StaffService();
         Staff newStaff = new Staff(3, "Pilot", 345);
-        //staffService.createStaff(newStaff);
-        //newStaff = staffService.getStaffByID(1);
         newStaff.setPosition("Worker");
-        //staffService.updateStaff(newStaff);
+        staffService.updateStaff(newStaff);
         staffService.deleteStaff(2);
-        //LOGGER.info(staffService.getAllStaff());
+        LOGGER.info(staffService.getAllStaff());
 
-        //json
-        MealService mealService = new MealService();
+
         JsonMapper jsonMapper = new JsonMapper();
         List<Meal> mealsList = jsonMapper.readJSON("src/main/resources/json/meals.json", Meal.class);
         jsonMapper.writeJSON(mealsList, "src/main/resources/json/new_meal.json");
-        //jaxb
-        mealService.getMealFromXML("src/main/resources/xml/meals.xml", "src/main/resources/xsd/meals.xsd");
-        mealService.createXML("src/main/resources/xml/meals.xml");
     }
 }
